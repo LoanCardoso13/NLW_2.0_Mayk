f07093746c80f8dcbd40445538e0eb6517b02db2
Loan Cardoso <jow13_122@yahoo.com>
2020-10-09 22:27:45 -0300

    UI finished

---
+++ registration.js
@@ -0,0 +1,11 @@
+// const reg = document.getElementById("registered");
+const modal = document.getElementById("modal")
+
+// reg.addEventListener("click", () => {
+//     toggleDisplay();
+// });
+
+
+function toggleDisplay() {
+    modal.style.display = "flex"
+};
