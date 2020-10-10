ddd757d7a2cf40fca40311aeab8ebe2078d7ff40
Loan Cardoso <jow13_122@yahoo.com>
2020-10-10 20:26:22 -0300

    Setting up server to run pages dynamically through nunjucks template engine

--- src/server.js
+++ src/server.js
@@ -1,22 +1,29 @@
 const express = require("express");
 const server = express();
 
+const nunjucks = require("nunjucks");
+
 server.use(express.static('public'));
 
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: false
+})
+
 server.get('/', (req, res) => {
-    return res.sendFile(__dirname + '/views/index.html');
+    return res.render('index.html');
 });
 
 server.get('/registration', (req, res) => {
-    return res.sendFile(__dirname + '/views/registration.html');
+    return res.render('registration.html');
 });
 
 server.get('/search-tutor', (req, res) => {
-    return res.sendFile(__dirname + '/views/search-results.html');
+    return res.render('search-results.html');
 });
 
 server.get('/registered', (req, res) => {
-    return res.sendFile(__dirname + '/views/modal.html');
+    return res.render('modal.html');
 }); 
 
 server.listen(3000);
