04f3e9c7b6848988e895cdc4b97b5d4ab9a9e24c
Loan Cardoso <jow13_122@yahoo.com>
2020-10-13 15:56:39 -0300

    Fixing UI details at registration page (improving input/select tags)

--- public/scripts/registration.js
+++ public/scripts/registration.js
@@ -19,32 +19,44 @@ function addRow() {
     labelElement3.appendChild(textElement3);
 
 
-    var inputElement1 = document.createElement('input');
-    inputElement1.setAttribute('type', 'text');
-    inputElement1.setAttribute('name', 'weekday');
+    var selectElement1 = document.createElement('select');
+    selectElement1.setAttribute('name', 'weekday');
+    selectElement1.setAttribute('onchange', 'changeColor2()');
+    selectElement1.innerHTML = `<option value="">Select weekday</option>`
+    for (weekday of weekdays) {
+        selectElement1.innerHTML += `<option value="${weekday}">${weekday}</option>`
+    }
 
-    var inputElement2 = document.createElement('input');
-    inputElement2.setAttribute('type', 'text');
-    inputElement2.setAttribute('name', 'from');
+    var selectElement2 = document.createElement('select');
+    selectElement2.setAttribute('name', 'from');
+    selectElement2.setAttribute('onchange', 'changeColor2()');
+    selectElement2.innerHTML = `<option value="">Start</option>`
+    for (time of times) {
+        selectElement2.innerHTML += `<option value="${time}">${time}</option>`
+    }
 
-    var inputElement3 = document.createElement('input');
-    inputElement3.setAttribute('type', 'text');
-    inputElement3.setAttribute('name', 'to');
+    var selectElement3 = document.createElement('select');
+    selectElement3.setAttribute('name', 'to');
+    selectElement3.setAttribute('onchange', 'changeColor2()');
+    selectElement3.innerHTML = `<option value="">End</option>`
+    for (time of times) {
+        selectElement3.innerHTML += `<option value="${time}">${time}</option>`
+    }
 
     var fieldElement1 = document.createElement('div');
     fieldElement1.setAttribute('class', 'field');
     fieldElement1.appendChild(labelElement1);
-    fieldElement1.appendChild(inputElement1);
+    fieldElement1.appendChild(selectElement1);
 
     var fieldElement2 = document.createElement('div');
     fieldElement2.setAttribute('class', 'field');
     fieldElement2.appendChild(labelElement2);
-    fieldElement2.appendChild(inputElement2);
+    fieldElement2.appendChild(selectElement2);
 
     var fieldElement3 = document.createElement('div');
     fieldElement3.setAttribute('class', 'field');
     fieldElement3.appendChild(labelElement3);
-    fieldElement3.appendChild(inputElement3);
+    fieldElement3.appendChild(selectElement3);
 
     var rowElement = document.createElement('div');
     rowElement.setAttribute('class', 'field-group-2');
@@ -53,4 +65,61 @@ function addRow() {
     rowElement.appendChild(fieldElement3);
 
     container.appendChild(rowElement);
+}
+
+topics = [ 
+    'Biology',
+    'Math',
+    'Physics',
+    'Chemistry',
+    'English',
+    'History',
+    'Geography'
+];
+
+weekdays = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday'
+];
+
+var times = []
+for (i = 0; i < 24; i++) {
+    times.push(String(i) + 'h');
+}
+
+const topicElement = document.querySelector("select[name=topic]");
+const weekdayElement = document.querySelector('select[name=weekday]');
+const fromElement = document.querySelector('select[name=from]');
+const toElement = document.querySelector('select[name=to]');
+
+for (topic of topics) {
+    topicElement.innerHTML += `<option value="${topic}">${topic}</option>`
+}
+
+for (weekday of weekdays) {
+    weekdayElement.innerHTML += `<option value="${weekday}">${weekday}</option>`
+}
+
+for (time of times) {
+    fromElement.innerHTML += `<option value="${time}">${time}</option>`
+}
+
+for (time of times) {
+    toElement.innerHTML += `<option value="${time}">${time}</option>`
+}
+
+function changeColor() {
+    var selectorElement = document.querySelector("form fieldset .field select");
+    selectorElement.classList.add("selected");
+}
+
+function changeColor2() {
+    var selectorElements = document.querySelectorAll("form fieldset .field-group-2 .field select");
+    console.log(selectorElements.length)
+    for (i = 0; i < selectorElements.length; i++) {
+        selectorElements[i].classList.add("selected");
+    }
 }
