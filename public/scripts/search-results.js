170b11ebb7ebed3f5da5e02c9288c70567ffae7b
Loan Cardoso <jow13_122@yahoo.com>
2020-10-26 17:36:33 -0300

    Cleaning code in registration page

--- public/scripts/search-results.js
+++ public/scripts/search-results.js
@@ -1,6 +1,6 @@
 const topicElement = document.querySelector('select[name=subject]');
 const weekdayElement = document.querySelector('select[name=weekday]');
-const timeElement = document.querySelector('select[name=time]');
+// const timeElement = document.querySelector('select[name=time]');
 
 topics = [ 
     'Biology',
@@ -20,10 +20,10 @@ weekdays = [
     'Friday'
 ];
 
-var times = []
-for (i = 0; i < 24; i++) {
-    times.push(String(i) + 'h');
-}
+// var times = []
+// for (i = 0; i < 24; i++) {
+//     times.push(String(i) + 'h');
+// }
 
 for (topic of topics) {
     topicElement.innerHTML += `<option value="${topic}">${topic}</option>`
@@ -33,9 +33,9 @@ for (weekday of weekdays) {
     weekdayElement.innerHTML += `<option value="${weekday}">${weekday}</option>`
 }
 
-for (time of times) {
-    timeElement.innerHTML += `<option value="${time}">${time}</option>`
-}
+// for (time of times) {
+//     timeElement.innerHTML += `<option value="${time}">${time}</option>`
+// }
 
 // weekdayElement.style.opacity = "1.0";
 // timeElement.style.opacity = "1.0";
